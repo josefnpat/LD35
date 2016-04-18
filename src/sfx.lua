07f7e2bb3ea70e3c0e543809ffc0c6898e4bc32e
Seppi <seppi@josefnpat.com>
2016-04-18 01:20:50 -0500

    Convert wav to ogg

--- src/sfx.lua
+++ src/sfx.lua
@@ -1,41 +1,41 @@
 local sfx = {
   boss = {
     spawn = {
-      love.audio.newSource("assets/sfx/boss_1.wav","static"),
-      love.audio.newSource("assets/sfx/boss_2.wav","static"),
-      love.audio.newSource("assets/sfx/boss_3.wav","static"),
-      love.audio.newSource("assets/sfx/boss_4.wav","static"),
-      love.audio.newSource("assets/sfx/boss_5.wav","static"),
+      love.audio.newSource("assets/sfx/boss_1.ogg","static"),
+      love.audio.newSource("assets/sfx/boss_2.ogg","static"),
+      love.audio.newSource("assets/sfx/boss_3.ogg","static"),
+      love.audio.newSource("assets/sfx/boss_4.ogg","static"),
+      love.audio.newSource("assets/sfx/boss_5.ogg","static"),
     },
     hurt = {
-      love.audio.newSource("assets/sfx/boss_hurt_1.wav","static"),
-      love.audio.newSource("assets/sfx/boss_hurt_2.wav","static"),
-      love.audio.newSource("assets/sfx/boss_hurt_3.wav","static"),
+      love.audio.newSource("assets/sfx/boss_hurt_1.ogg","static"),
+      love.audio.newSource("assets/sfx/boss_hurt_2.ogg","static"),
+      love.audio.newSource("assets/sfx/boss_hurt_3.ogg","static"),
     },
     kill = {
-      love.audio.newSource("assets/sfx/boss_kill.wav","static"),
+      love.audio.newSource("assets/sfx/boss_kill.ogg","static"),
     },
   },
   player = {
     spawn = {
-      love.audio.newSource("assets/sfx/player_1.wav","static"),
-      love.audio.newSource("assets/sfx/player_2.wav","static"),
-      love.audio.newSource("assets/sfx/player_3.wav","static"),
-      love.audio.newSource("assets/sfx/player_4.wav","static"),
-      love.audio.newSource("assets/sfx/player_5.wav","static"),
+      love.audio.newSource("assets/sfx/player_1.ogg","static"),
+      love.audio.newSource("assets/sfx/player_2.ogg","static"),
+      love.audio.newSource("assets/sfx/player_3.ogg","static"),
+      love.audio.newSource("assets/sfx/player_4.ogg","static"),
+      love.audio.newSource("assets/sfx/player_5.ogg","static"),
     },
     hurt = {
-      love.audio.newSource("assets/sfx/player_hurt_1.wav","static"),
-      love.audio.newSource("assets/sfx/player_hurt_2.wav","static"),
-      love.audio.newSource("assets/sfx/player_hurt_3.wav","static"),
+      love.audio.newSource("assets/sfx/player_hurt_1.ogg","static"),
+      love.audio.newSource("assets/sfx/player_hurt_2.ogg","static"),
+      love.audio.newSource("assets/sfx/player_hurt_3.ogg","static"),
     },
     kill = {
-      love.audio.newSource("assets/sfx/player_kill.wav","static"),
+      love.audio.newSource("assets/sfx/player_kill.ogg","static"),
     },
   },
   weapon = {
-    { love.audio.newSource("assets/sfx/weapon_1.wav","static"), },
-    { love.audio.newSource("assets/sfx/weapon_2.wav","static"), },
+    { love.audio.newSource("assets/sfx/weapon_1.ogg","static"), },
+    { love.audio.newSource("assets/sfx/weapon_2.ogg","static"), },
   },
 }
 
